f033a182e7033ef9ea334250b65fdb972a1bdf17
FlorianDarques <107691809+FlorianDarques@users.noreply.github.com>
2022-11-08 17:41:30 +0100

    inscription paramètre
    empêcher deux fois la même adresse mail de s'inscrire

--- inscription.php
+++ inscription.php
@@ -9,22 +9,19 @@
             $db = new PDO($dsn, DBUSER, DBPASS);
             $db->exec("SET NAMES utf8");
             $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            $email = $_POST['email'];
-            $username = $_POST['username'];
-            $pswd = $_POST['pswd'];
+            $email = htmlentities($_POST['email']);
+            $username = htmlentities($_POST['username']);
+            $pswd = htmlentities(password_hash($_POST['pswd'], PASSWORD_DEFAULT, ['cost' => 12]));
             if(!empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['pswd'])) {
-                $sql = "INSERT INTO `membre`(email, username, pswd) VALUES (:email, :username, :pswd)";
+                $sql = "INSERT INTO `membre`(email, username, pswd) VALUES ('$email', '$username', '$pswd')";
                 $query = $db->prepare($sql);
-                $query->bindValue(":email", $email, PDO::PARAM_STR);
-                $query->bindValue(":username", $username, PDO::PARAM_STR);
-                $query->bindValue(":pswd", $pswd, PDO::PARAM_STR);
                 $query->execute();
-                $userinfo = $query->fetchAll();
-        };
+            };
+        }
+        catch(PDOException $e) {
+            die($e->getMessage());
+        }
     }
-    catch(PDOException $e) {
-    die($e->getMessage());
-    }
-}
-header('location: index.php')
+    header('location: inscriptionform.php');
 ?>
+
---
+++ inscriptionform.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>TraCalorie</title>
+</head>
+
+<body>
+    <div class="content flex">
+        <h1>Inscription</h1>
+        <form action="inscription.php" method="POST" class="flex content">
+            <div>
+                <input type="text" placeholder="Adresse Mail" name="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" autocomplete="off">
+            </div>
+            <div><input type="text" placeholder="Nom d'utilisateur" name="username" autocomplete="off">
+            </div>
+            <div><input type="password" placeholder="Mot de passe" name="pswd" autocomplete="off">
+            </div>
+            <div><button type="submit" name="submit">valider</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
+
+<?php
+
+?>
